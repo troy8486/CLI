35b48438cd1e56948de56bb835386bdf75e723fa
troy8486 <rtanmoy262@gmail.com>
2024-02-08 16:52:05 +0530

    msg- up to bulma

---
+++ tbook/src/index.tsx
@@ -0,0 +1,89 @@
+import 'bulmaswatch/superhero/bulmaswatch.min.css'
+import * as esbuild from 'esbuild-wasm';
+import { useState, useEffect, useRef } from 'react';
+import ReactDOM from 'react-dom/client';
+import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
+import { fetchPlugin } from './plugins/fetch-plugin';
+import CodeEditor from './components/code-editor';
+
+const el = document.getElementById('root');
+
+const root = ReactDOM.createRoot(el!);
+
+const App = () => {
+  const ref = useRef<any>();
+  const iframe = useRef<any>();
+  const [input, setInput] = useState('');
+
+  const startService = async () => {
+    ref.current = await esbuild.startService({
+      worker: true,
+      wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm',
+    });
+  };
+  useEffect(() => {
+    startService();
+  }, []);
+
+  const onClick = async () => {
+    if (!ref.current) {
+      return;
+    }
+
+    iframe.current.srcDoc = html; // resetting iframe as soon as submit is clicked, whatif user deletes the root so we need to reset it
+
+    const result = await ref.current.build({
+      entryPoints: ['index.js'],
+      bundle: true,
+      write: false,
+      plugins: [unpkgPathPlugin(), fetchPlugin(input)],
+      define: {
+        'process.env.NODE_ENV': '"production"',
+        global: 'window',
+      },
+    });
+
+    // setCode(result.outputFiles[0].text);
+    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*');
+  };
+
+  const html = `
+    <html>
+      <head></head>
+      <body>
+        <div id="root"></div>
+        <script>
+          window.addEventListener('message', (event) => {
+            try{
+                eval(event.data);
+            } catch(err){
+                const root = document.querySelector('#root');
+                root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>'
+                console.error(err);
+            }
+            
+          }, false);
+        </script>
+      </body>
+    </html>
+  `;
+
+  return (
+    <div>
+      <CodeEditor 
+        initialValue='hello'
+        onChange = {(value)=> setInput(value)}
+      />
+      <textarea
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      ></textarea>
+      <div>
+        <button onClick={onClick}>Submit</button>
+      </div>
+      <iframe title="codepreview" ref={iframe} sandbox="allow-scripts" srcDoc={html} />
+    </div>
+  );
+};
+
+root.render(<App />);
